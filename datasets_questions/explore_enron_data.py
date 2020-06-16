f27b456576add9bd028bf0dd4d29084dbb64d95d
Xinan <taoxinan@gmail.com>
2020-06-16 16:23:17 +0100

    data set play

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,7 +12,7 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
@@ -20,11 +20,13 @@ import pickle
 enron_data = pickle.load(
     open("../final_project/final_project_dataset.pkl", "rb"))
 
-print(len(enron_data))
+data_length = len(enron_data)
+
+print('data length', data_length)
 
 print('example', enron_data["SKILLING JEFFREY K"])
 
-print(len(enron_data["SKILLING JEFFREY K"].keys()))
+# print(len(enron_data["SKILLING JEFFREY K"].keys()))
 
 pois = {name: features for (name, features)
         in enron_data.items() if features['poi'] == 1}
@@ -47,3 +49,33 @@ poi_names_filtered = [k for k in poi_names if any(
     k.split()[0].capitalize() in n for n in names_trimmed_list)]
 
 print('poi_names_filtered', len(poi_names_filtered))
+
+print('james stock value', enron_data['PRENTICE JAMES']['total_stock_value'])
+
+print('wesley emails from poi',
+      enron_data['COLWELL WESLEY']['from_this_person_to_poi'])
+
+print('stock exerised by jeffrey',
+      enron_data["SKILLING JEFFREY K"]['exercised_stock_options'])
+
+print('how many folks have qualified salary', len(
+    [k for k, v in enron_data.items() if v['salary'] != 'NaN']))
+
+print('how many folks have email addresses', len(
+    [k for k, v in enron_data.items() if v['email_address'] != 'NaN']))
+
+people_without_total_payments = len(
+    [k for k, v in enron_data.items() if v['total_payments'] == 'NaN'])
+
+print('how many folks do not have total payments', people_without_total_payments)
+
+print('percentage of that', float(
+    people_without_total_payments) / float(data_length))
+
+poi_without_payments = [k for k, v in enron_data.items(
+) if v['total_payments'] == 'NaN' and v['poi'] == 1]
+
+print('debug', poi_without_payments)
+
+# print('percentage of poi without total payments',
+#       float(len(poi_without_payments)) / float(data_length))
