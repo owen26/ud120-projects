ab84bf2af5a2dc3b33727444dd2460f00c95eda7
Xinan <taoxinan@gmail.com>
2020-06-16 11:32:47 +0100

    change

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -22,9 +22,28 @@ enron_data = pickle.load(
 
 print(len(enron_data))
 
+print('example', enron_data["SKILLING JEFFREY K"])
+
 print(len(enron_data["SKILLING JEFFREY K"].keys()))
 
 pois = {name: features for (name, features)
         in enron_data.items() if features['poi'] == 1}
 
 print('poi', len(pois))
+
+names_list = open("../final_project/poi_names.txt",
+                  "r").read().splitlines()[2:]
+
+names_trimmed_list = [x[4:] for x in names_list]
+
+poi_names = [k for k, v in enron_data.items() if v['poi'] ==
+             1]
+
+# print('names', names_trimmed_list)
+
+# print('poi names', poi_names)
+
+poi_names_filtered = [k for k in poi_names if any(
+    k.split()[0].capitalize() in n for n in names_trimmed_list)]
+
+print('poi_names_filtered', len(poi_names_filtered))
